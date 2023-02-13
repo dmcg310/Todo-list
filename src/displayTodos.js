de57b80d6e3f4aa492da58dc2b1f55e9720b2760
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-02-13 18:09:23 +0000

    feat: date formatting, css

--- src/displayTodos.js
+++ src/displayTodos.js
@@ -1,11 +1,14 @@
+import { formatDistance, parseISO, subDays } from "date-fns";
+
 function displayTodos(title, description, dueDate, priority) {
+  let dateToday = new Date();
+  let difference = formatDistance(dateToday, parseISO(dueDate));
   const todoList = document.getElementById("todo-list");
   todoList.innerHTML += `<h3 class='todo-item'> 
-      ${title} 
-      ${description} 
-      ${dueDate} 
-      ${priority} 
-      <input id="complete" type="checkbox"></input>
+      ${title}\u00A0\u00A0\u00A0 
+      ${description}\u00A0\u00A0
+      Due: ${dueDate} (${difference})\u00A0\u00A0  
+      ${priority} priority 
       <button id="removeTodo" type="submit">Remove Todo</button>
     </h3>`;
   removeTodo();
--- src/page_load.js
+++ src/page_load.js
@@ -4,15 +4,9 @@ function loadup() {
   const header = document.getElementById("header");
   const headerText = document.createElement("div");
   headerText.id = "headerText";
-  headerText.innerHTML = "<h1>Todo List</h1>";
+  headerText.innerHTML = `<h1>Todo List</h1>
+    <h3>Your Todos</h3>`;
   header.append(headerText);
-
-  const container = document.getElementById("container");
-  const containerContent = document.createElement("div");
-  containerContent.id = "containerContent";
-  containerContent.innerHTML = "<h3 class='todoTitle'>Your todo's</h3>";
-
-  container.append(containerContent);
 
   buttonCreation();
 }
