c6f8f2d38675ebf71a00359468561324ada4675d
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-02-14 21:24:38 +0000

    feat: mobile responsiveness

--- src/displayTodos.js
+++ src/displayTodos.js
@@ -8,7 +8,7 @@ function displayTodos(title, description, dueDate, priority) {
       ${title}\u00A0\u00A0\u00A0| 
       ${description}\u00A0\u00A0|
       Due: ${dueDate}\u00A0\u00A0| 
-      ${priority} priority |
+      ${priority} priority
       <button id="removeTodo" type="submit"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-checks" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
       <path d="M7 12l5 5l10 -10" />
@@ -24,7 +24,7 @@ function displayTodos(title, description, dueDate, priority) {
       ${title}\u00A0\u00A0\u00A0|
       ${description}\u00A0\u00A0|
       Due: ${dueDate} (${difference})\u00A0\u00A0|
-      ${priority} priority | 
+      ${priority} priority 
       <button id="removeTodo" type="submit"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-checks" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
       <path d="M7 12l5 5l10 -10" />
