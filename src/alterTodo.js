18e39bd90513796f8abdddbc910180e63b3b3176
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-02-17 11:56:30 +0000

    feature: form validation; blank fields

--- src/alterTodo.js
+++ src/alterTodo.js
@@ -1,32 +1,37 @@
 import { arrayToSeperateValues } from "./todoLogic.js";
 
 function alterTodo() {
-	let todoArray = [];
-	const addBtn = document.querySelector(".newTodoButton");
+  let todoArray = [];
+  const addBtn = document.querySelector(".newTodoButton");
 
-	addBtn.addEventListener("click", () => {
-		const form = document.getElementById("form-modal");
-		form.style.display = "block";
+  addBtn.addEventListener("click", () => {
+    const form = document.getElementById("form-modal");
+    form.style.display = "block";
 
-		const submit = document.querySelector(".submitForm");
-		submit.addEventListener("click", function (event) {
-			event.stopImmediatePropagation();
-			event.preventDefault();
+    const submit = document.querySelector(".submitForm");
+    submit.addEventListener("click", function (event) {
+      event.stopImmediatePropagation();
+      event.preventDefault();
 
-			const title = document.getElementById("title").value;
-			const description = document.getElementById("description").value;
-			const dueDate = document.getElementById("dueDate").value;
-			const priority = document.getElementById("priority").value;
-			const todoForm = document.getElementById("form");
+      const title = document.getElementById("title").value;
+      const description = document.getElementById("description").value;
+      const dueDate = document.getElementById("dueDate").value;
+      const priority = document.getElementById("priority").value;
+      const todoForm = document.getElementById("form");
 
-			todoForm.reset();
-			form.style.display = "none";
+      if (title === "" || description === "" || priority === "") {
+        alert("Title, description, or priority fields cannot be left blank.");
+        return alterTodo()
+      }
 
-			todoArray.push(title, description, dueDate, priority);
-			arrayToSeperateValues(todoArray);
-			todoArray = [];
-		});
-	});
+      todoForm.reset();
+      form.style.display = "none";
+
+      todoArray.push(title, description, dueDate, priority);
+      arrayToSeperateValues(todoArray);
+      todoArray = [];
+    });
+  });
 }
 
 export { alterTodo };
