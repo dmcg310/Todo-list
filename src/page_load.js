4ec7223795a36928d18c97dbd5b0ebc83e994271
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-02-21 11:56:57 +0000

    feat: dark mode toggle

--- src/page_load.js
+++ src/page_load.js
@@ -1,30 +1,40 @@
 import { buttonCreation } from "./buttonCreation.js";
 
 function loadup() {
-	const header = document.getElementById("header");
-	const headerText = document.createElement("div");
-	const darkModeButton = document.createElement("button");
+  const header = document.getElementById("header");
+  const headerText = document.createElement("div");
+  const darkModeButton = document.createElement("button");
 
-	darkModeButton.classList = "darkModeButton";
-	darkModeButton.type = "submit";
-	darkModeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  darkModeButton.classList = "darkModeButton";
+  darkModeButton.type = "submit";
+  darkModeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
 </svg>`;
+  darkModeButton.addEventListener("click", darkMode());
 
-	headerText.id = "headerText";
-	headerText.innerHTML = `<h1 class="header-title">Todo List</h1>
+  function darkMode() {
+    darkModeButton.addEventListener("click", function () {
+      const container = document.getElementById("container");
+      const sidebar = document.getElementById("sidebar");
+      container.classList.toggle("dark-mode");
+      sidebar.classList.toggle("dark-mode-lighter");
+    });
+  }
+
+  headerText.id = "headerText";
+  headerText.innerHTML = `<h1 class="header-title">Todo List</h1>
     <h3 class="header-subtitle">Your Todos</h3>`;
-	header.append(headerText, darkModeButton);
+  header.append(headerText, darkModeButton);
 
-	buttonCreation();
+  buttonCreation();
 
-	let modal = document.getElementById("form-modal");
-	window.onclick = function (event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	};
+  let modal = document.getElementById("form-modal");
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 }
 
 export { loadup };
