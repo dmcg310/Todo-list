e10b2d452f2d34f5a5f1dd19fe80c13f668c63be
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-02-22 21:02:16 +0000

    feat: dark mode now saves in local storage

--- src/page_load.js
+++ src/page_load.js
@@ -14,15 +14,33 @@ function loadup() {
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
 </svg>`;
+
 	darkModeButton.addEventListener("click", darkMode());
 
 	function darkMode() {
 		darkModeButton.addEventListener("click", function () {
 			const container = document.getElementById("container");
 			const sidebar = document.getElementById("sidebar");
+
 			container.classList.toggle("dark-mode");
 			sidebar.classList.toggle("dark-mode-lighter");
+
+			if (localStorage.getItem("darkMode") === "true") {
+				localStorage.setItem("darkMode", "false");
+			} else {
+				localStorage.setItem("darkMode", "true");
+			}
 		});
+	}
+
+	function darkModeOnRender() {
+		if (localStorage.getItem("darkMode") === "true") {
+			const container = document.getElementById("container");
+			const sidebar = document.getElementById("sidebar");
+
+			container.classList.toggle("dark-mode");
+			sidebar.classList.toggle("dark-mode-lighter");
+		}
 	}
 
 	headerText.id = "headerText";
@@ -45,6 +63,8 @@ function loadup() {
 			clearModal.style.display = "none";
 		}
 	};
+
+	darkModeOnRender();
 }
 
 export { loadup };
